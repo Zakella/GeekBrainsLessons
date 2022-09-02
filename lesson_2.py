15532b99630953e6e69e11580f67b5ac98a8ba98
Zakella <zapolski.slava@gmail.com>
2022-09-02 10:49:05 +0300

    2 ый урок 1.2

--- lesson_2.py
+++ lesson_2.py
@@ -1,9 +1,9 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = int(input("Введите x: "))
-y = int(input("Введите y: "))
-z = int(input("Введите z: "))
+x = input("Введите x: ")
+y = input("Введите y: ")
+z = input("Введите z: ")
 
-expression1 = x + y + z
-expression2 = x * y
+expression1 = not (x or y or z)
+expression2 = not x and not y and not z
 print(expression1 == expression2)
