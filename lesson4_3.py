75be9894a0e1d7833588119bd891753fbd6dc5fb
Zakella <zapolski.slava@gmail.com>
2022-09-13 11:01:28 +0300

    6.3

---
+++ lesson4_3.py
@@ -0,0 +1,12 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+some_list = [1, 5, 5, 7, 12, -3, 1, -3, 8, 7]
+print(list(set(some_list)))#Первый способ
+
+unic_list = []
+
+for i in some_list:
+    if i not in unic_list:
+        unic_list.append(i)
+
+print(unic_list)  # второй лист
